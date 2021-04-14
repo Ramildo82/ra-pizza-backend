323cf4b023b8c40a8c44c69ea54551a09e301336
Ramildo82 <53921093+Ramildo82@users.noreply.github.com>
2021-04-14 20:19:24 -0300

    v_2

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-const express = require('express');
-const config = require('config');
+const express = require('express')
+const config = require('config')
+const pg = require('pg')
+const port = process.env.PORT || config.get("server.port")
 
-const port = process.env.PORT || config.get('server.port')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const listaPedidos = [
-    {}
-]
+const listaPedidos = []
+const pool = new pg.Pool ({
+    connectionString: "postgres://swfoyrjzqjcbsb:59d025ca89214149926b40da06602cb6b5cd6846ce5781d217139791af670e82@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d67g46rslegabv",
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
-app.route('/pedidos').get( (req, res)=> {
+app.route('/reset').get((req, res) => { 
+    let qry = "DROP TABLE IF EXISTS pedidos;"
+    qry += "CREATE TABLE pedidos ("
+    qry += "cliente char(200),"
+    qry += "sabor char(50),"
+    qry += "quantidade int,"
+    qry += "tamanho char(25)"
+    qry += ");"
+    pool.query(qry, (err, dbres) => {
+        if (err) { 
+            res.status(500).send(err)
+        } else { 
+            res.status(200).send("Banco de dados resetado")
+        }
+    })
+})
+
+app.route('/pedidos').get((req, res) => {
     console.log("/pedidos acionado")
-    res.status(200).json(listaPedidos)
+    let qry = "SELECT * FROM pedidos;"
+    pool.query(qry, (err, dbres) => { 
+        if(err) { 
+            res.status(500).send(err)
+        } else { 
+            res.status(200).json(dbres.rows)
+        }
+    });
 })
 
-app.route("/pedidos/adicionar").post( (req, res) => {    
-    listaPedidos.push(req.body)   
-    res.status(200).send("Pedido adicionado com sucesso");
+app.route('/pedido/adicionar').post((req, res) => { 
+    console.log("/pedido/adicionar acionado")
+    let qry = "INSERT INTO pedidos (cliente, sabor, quantidade, tamanho) "
+    qry += ` VALUES ('${req.body.cliente}', '${req.body.sabor}', ${req.body.quantidade}, '${req.body.tamanho}');`
+    pool.query(qry, (err, dbres) => { 
+        if (err) { 
+            res.status(500).send(err)
+        } else { 
+            res.status(200).send("Pedido adicionado com sucesso")
+        }
+    });
 })
 
-app.listen(port, ()=> {
-    console.log("Inicio o servidor na porta ", port)
+app.listen(port, () => { 
+    console.log("Iniciando o servidor na porta ", port)
 })
 
 console.log("Inicio do projeto")
